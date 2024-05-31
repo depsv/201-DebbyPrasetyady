5e0b851dda683633edec3e5924d835e758143d22
Reza Maulana <7392maulana@gmail.com>
2024-05-31 21:53:37 +0700

    Update OrdersComponent.php

--- app/Livewire/OrdersComponent.php
+++ app/Livewire/OrdersComponent.php
@@ -56,9 +56,6 @@ class OrdersComponent extends Component
 
     public function submitOrders()
     {
-        if (empty($this->orders)) {
-            return;
-        }
 
         \DB::beginTransaction();
 
