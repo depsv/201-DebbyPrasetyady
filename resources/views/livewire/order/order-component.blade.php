06717fc44dd3fc1b3e07794e9516542438196160
Reza Maulana <7392maulana@gmail.com>
2024-05-31 21:36:31 +0700

    Update order-component.blade.php
    test fix

--- resources/views/livewire/order/order-component.blade.php
+++ resources/views/livewire/order/order-component.blade.php
@@ -11,7 +11,7 @@
                 <div class="p-6 lg:p-8 bg-white dark:bg-gray-800 dark:bg-gradient-to-bl dark:from-gray-700/50 dark:via-transparent border-b border-gray-200 dark:border-gray-700">
                     <h1 class="text-2xl font-medium text-gray-900 dark:text-white">Create Order</h1>
                     <div class="mt-4">
-                        <form wire:submit.prevent="addOrder">
+                        <form wire:submit="addOrder">
                             @csrf
                             <div class="flex flex-wrap -mx-3 mb-6">
                                 <div class="w-full md:w-1/2 px-3">
